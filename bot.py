c1015734ce6e55de0271dd19cedd062014c406b4
ThomasJThomas-podcast <touma@tmsk.jp>
2025-07-20 03:41:53 +0900

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,17 @@
+import os
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong!")
+
+token = os.environ["DISCORD_TOKEN"]   # Render で後から登録
+bot.run(token)
